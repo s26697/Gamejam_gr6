0694f0293076e819bfdc09bb484050abb49c312b
truew1n <48839784+truew1n@users.noreply.github.com>
2024-11-30 19:45:16 +0100

    Rename

--- Assets/Scripts/Hud/HealthComponent.cs
+++ Assets/Scripts/Hud/HealthComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBarLogic : MonoBehaviour
+public class HealthComponent : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
